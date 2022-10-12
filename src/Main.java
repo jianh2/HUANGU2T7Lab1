c3cbbfa0310f90934a28fa04b7fb3dd9f2d6d1d1
Jian Huang <jianh2@nycstudents.net>
2022-10-12 09:29:05 -0400

    U2T7 Lab1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String args[]) {
+
+        String str1 = "Hello!";
+        String str2 = "Hello!";
+        String str3 = "Hello!";
+        String str4 = "Hello! ";
+        String str5 = new String("Hello!");
+        System.out.println(str1 == str2);
+        System.out.println(str1 == str3);
+        System.out.println(str1 == str4);
+        System.out.println(str1 == str5);
+        Syse
+
+
+
+
+
+
+    }
+}
