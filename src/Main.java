5ee7c9c007e1119ef858365e306fda8ac591e17a
Jianh <jianh2@Nycstudents.ent>
2022-10-17 23:44:58 -0400

    U2T7 Lab1

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,47 @@ public class Main {
         System.out.println("Please enter a word");
         String str = scan.nextLine();
         System.out.println("Your word is " + str.length() + " letters long");
-        Int length = str.length();
-        if ((str.length() % 2) > 0)
-        System.out.println("The first half of your word is " +
+        int length = str.length();
+        if ((str.length() % 2) > 0) {
+            System.out.println("The first half of your word is " + str.substring(0, length / 2));
+            System.out.println("The second half of your word is " + str.substring(length / 2 + 1));
+        } else {
+            System.out.println("The first half of your word is " + str.substring(0, length / 2));
+            System.out.println("The second half of your word is " + str.substring(length / 2));
 
 
+        }
+        System.out.println("Please enter a second word");
+        String str2 = scan.nextLine();
+        if (str.equals(str)) {
+            System.out.println("The two words you entered are the same");
 
 
+        }
+
+        if (str.compareTo(str2) > 0) {
+            System.out.println("The first word comes after  the second word");
+        } else {
+            System.out.println("The second word comes after the first word");
+        }
+
+        if (str.length() > str2.length()) {
+            System.out.println("The first word is longer than the second word");
+        } else {
+            System.out.println("The second word is longer than the first word");
+        }
+
+        if ( str.indexOf(str2) > -1) {
+            System.out.println("The first word can be found in the second word at index" + str.indexOf(str2));
+        } else {
+            System.out.println("not found");
+        }
 
 
-
+        if ( str2.indexOf(str) > -1) {
+            System.out.println("The second word can be found in the first word at index" + str2.indexOf(str));
+        } else {
+            System.out.println("not found");
+        }
     }
 }
